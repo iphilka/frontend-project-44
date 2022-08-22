4e761b25dcd6ec97d5fed743973338c35ba2542c
iphilka <i376765616@yandex.ru>
2022-08-22 11:58:32 +0500

    step 8

---
+++ bin/brain-progression.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import gameLauncher from '../src/games/progression_game.js';
+
+gameLauncher();
---
+++ src/games/progression_game.js
@@ -0,0 +1,26 @@
+import { askQuestion, randomInteger, createNewGame } from '../index.js';
+
+const getProgression = (firstNumber, step) => {
+  const progressionLength = randomInteger(6, 15);
+  const progression = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(firstNumber + step * 1);
+  }
+  return progression;
+};
+
+const gameLauncher = createNewGame(() => {
+  const firstNumber = randomInteger(0, 50);
+  const step = randomInteger(2, 8);
+  const progression = getProgression(firstNumber, step);
+  const randomIndex = randomInteger(0, progression.length - 1);
+  const calculateAnswer = progression[randomIndex];
+  progression[randomIndex] = '..';
+  const gameNumber = progression.join(' ');
+
+  const userAnswer = askQuestion(`What number is missing in the progression?\nQuestion: ${gameNumber}`);
+
+  return [userAnswer, calculateAnswer];
+});
+
+export default gameLauncher;
